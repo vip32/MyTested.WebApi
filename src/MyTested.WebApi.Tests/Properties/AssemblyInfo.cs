e13bb40bb29827be4f10767aaf475fbc6259002b
Vincent van Proosdij <mini32@gmail.com>
2018-07-02 17:25:54 +0200

    version

--- src/MyTested.WebApi.Tests/Properties/AssemblyInfo.cs
+++ src/MyTested.WebApi.Tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿// MyTested.WebApi - ASP.NET Web API Fluent Testing Framework
 // Copyright (C) 2015 Ivaylo Kenov.
-// 
+//
 // Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -9,10 +9,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Unit tests for ASP.NET Web API Fluent Testing Framework")]
 [assembly: AssemblyCompany("Ivaylo Kenov")]
 [assembly: AssemblyProduct("MyTested.WebApi.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("29f6a15e-c6d1-4c93-83c8-18ff839950e8")]
 
-[assembly: AssemblyVersion("1.2.6.0")]
-[assembly: AssemblyFileVersion("1.2.6.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
--- src/MyTested.WebApi/Properties/AssemblyInfo.cs
+++ src/MyTested.WebApi/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿// MyTested.WebApi - ASP.NET Web API Fluent Testing Framework
 // Copyright (C) 2015 Ivaylo Kenov.
-// 
+//
 // Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -9,10 +9,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("ASP.NET Web API Fluent Testing Framework")]
 [assembly: AssemblyCompany("Ivaylo Kenov")]
 [assembly: AssemblyProduct("MyTested.WebApi")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("9e8264b0-67bf-4981-86fd-321f003c1d67")]
 
-[assembly: AssemblyVersion("1.2.6.0")]
-[assembly: AssemblyFileVersion("1.2.6.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
